35d36473363916d98c2c04cc04f067839c2cc03a
Steffen <steffen@indigo-nms.eu>
2020-07-02 00:03:39 +0200

    Convert to module

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,2 @@
-require = require("esm")(module, {
-    'cjs': {
-        'dedefault': true
-    }
-})
-module.exports = require("./CloudConvert.js")
+import CloudConvert from './CloudConvert'
+export default CloudConvert
