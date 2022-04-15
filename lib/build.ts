c163c54159e8a52231f84df9b20ec059dd940847
KnorpelSenf <shtrog@gmail.com>
2022-04-15 12:31:49 +0200

    Add npm build script

---
+++ lib/build.ts
@@ -0,0 +1,31 @@
+// ex. scripts/build_npm.ts
+import { build, emptyDir } from 'https://deno.land/x/dnt@0.23.0/mod.ts';
+
+await emptyDir('./npm');
+
+await build({
+    entryPoints: ['./mod.ts'],
+    outDir: './npm',
+    shims: {
+        // see JS docs for overview and more options
+        deno: true
+    },
+    package: {
+        // package.json properties
+        name: 'cloudconvert',
+        version: Deno.args[0],
+        description: 'Official Node.js SDK for the CloudConvert API',
+        license: 'MIT',
+        repository: {
+            type: 'git',
+            url: 'https://github.com/cloudconvert/cloudconvert-node.git'
+        },
+        bugs: {
+            url: 'https://github.com/cloudconvert/cloudconvert-node/issues'
+        }
+    }
+});
+
+// post build steps
+Deno.copyFileSync('LICENSE', 'npm/LICENSE');
+Deno.copyFileSync('README.md', 'npm/README.md');
